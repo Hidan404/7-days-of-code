c46a511a0a7b2f4369d2b11d79c1bbfa861406f3
Hidan404 <ronaldkurouzo@gmail.com>
2023-08-14 15:45:52 -0300

    fdfd

---
+++ CSS/imc.js
@@ -0,0 +1,18 @@
+
+let alunos = ["fabio", "ronald", "sabrina"];
+
+alunos[3] = 'hidan';
+alunos[4] = 'kisame';
+alunos[5] = 'zetsu';
+alunos.push('kakuzu', 'pain');
+let idexAlunos = alunos[1];
+alunos[1] = 'konan';
+console.log(alunos, idexAlunos);
+console.log(alunos.length);
+
+
+/*let num1 = 16.5;
+if (num1 >= 16 && num1 <= 17){
+    console.log('vc esta muito abaixo do peso');
+}*/
+    
